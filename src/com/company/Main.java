63394456ce696c067042aece13eb8627f16629db
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-09-07 17:19:02 -0400

    yeah

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner kbString = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+	    String fName = kbString.next();
+        System.out.println("Enter your last name");
+	    String lName = kbString.next();
+        System.out.println("Your full name is: " + fName + " " + lName);
+
+
     }
 }
